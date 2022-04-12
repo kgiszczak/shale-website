e8b5cf9e6de3edc84ee0684746118fff829413e8
Kamil Giszczak <beerkg@gmail.com>
2022-04-12 13:43:39 +0200

    Fix incorrect type used in examples

--- src/website/snippets/example-ruby-to-data/xml.rb
+++ src/website/snippets/example-ruby-to-data/xml.rb
@@ -9,7 +9,7 @@ end
 class Person < Shale::Mapper
   attribute :first_name, Shale::Type::String
   attribute :last_name, Shale::Type::String
-  attribute :age, Shale::Type::Date
+  attribute :age, Shale::Type::Integer
   attribute :married, Shale::Type::Boolean, default: -> { false }
   attribute :hobbies, Shale::Type::String, collection: true
   attribute :address, Address
